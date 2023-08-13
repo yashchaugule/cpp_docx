c0990eb297a0875102ed4d7ae8408d8acaad30a1
Yash Chaugule <125192060+yashchaugule@users.noreply.github.com>
2023-08-13 20:47:47 +0530

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{
+        float area,r,pi=3.14;
+        cout<<"Enter the radius of the circle:\n";
+        area=pi*r*r;
+        cin>>r;
+        
+        cout<<"Area="<<area<<endl;
+}
