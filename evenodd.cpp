b01325178a632755b1927300132c8128b014083c
Yash Chaugule <125192060+yashchaugule@users.noreply.github.com>
2023-08-13 20:34:55 +0530

    Create evenodd.cpp

---
+++ evenodd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  int n;
+
+  cout << "Enter the a number: ";
+  cin >> n;
+
+  if(n%2==0)
+  {
+          cout<<n<<" is an Even number";
+  }
+  else
+  {
+          cout<<n<<" is a Odd number";
+  }
+}
